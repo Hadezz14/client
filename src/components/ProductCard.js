e9b885ae7faa69017b1049902e9c7587806b2bd0
Hadezz14 <83659778+Hadezz14@users.noreply.github.com>
2023-08-31 13:17:20 +0545

    changes

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,98 +1,253 @@
+// import React from "react";
+// import ReactStars from "react-rating-stars-component";
+// import { Link, useLocation, useNavigate } from "react-router-dom";
+// import wish from "../images/wish.svg";
+// import tshirt2 from "../images/vyamtshirt.png";
+// import addcart from "../images/add-cart.svg";
+// import view from "../images/view.svg";
+// import { useDispatch } from "react-redux";
+// import { addToWishlist } from "../features/products/productSlice";
+
+// const ProductCard = (props) => {
+//   const {data, grid} = props;
+//   const dispatch = useDispatch();
+//   let location = useLocation();
+//   const navigate = useNavigate();
+ 
+//   const addToWish =(id) =>{
+//     dispatch(addToWishlist(id));
+//   };
+
+//   return (
+//     <>
+//       {data?.map((item,index) => {
+//         return(
+//           <div
+//             key={index}
+//             className={` ${
+//               location.pathname === "/product" ? `gr-${grid}` : "col-3"
+//             } `}
+//           >
+        
+//           <div
+//           className="product-card position-relative"
+//         >
+//           <div className="wishlist-icon position-absolute">
+//             <button 
+//               className="border-0 bg-transparent" 
+//               onClick={(e) =>{
+//                 addToWish(item?._id);
+//               }}>
+//               <img src={wish} alt="wishlist" />
+//             </button>
+//           </div>
+//           <Link
+//              to={`/product/${item._id}`}
+//           >
+//           <div className="product-image">
+//           {
+//             item?.images && item.images[0]&&(
+//             <img src={item?.images[0].url} className="img-fluid" alt="product image" />
+//               )
+//           }
+//           <img src={item?.images[0].url} className="img-fluid" alt="product image" />
+//           </div>
+//           </Link>
+//           <div className="product-details">
+//             <h6 className="brand">{item?.brand}</h6>
+//             <Link
+//               to={`/product/${item._id}`}
+//             > 
+//             <h5 className="product-title">
+//              {item?.title}
+//             </h5>
+//             </Link>
+            
+//             <ReactStars
+//               count={5}
+//               size={24}
+//               value={item?.totalrating.toString()}
+//               edit={false}
+//               activeColor="#ffd700"
+//             />
+//             <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}
+//               dangerouslySetInnerHTML={{__html:item?.description }}
+//             >
+//             </p>
+//             <p className="price">Rs {item?.price}</p>
+//           </div>
+//           <div className="action-bar position-absolute">
+//             <div className="d-flex flex-column gap-15">
+//               <button className="border-0 bg-transparent">
+//                 <img onClick={()=>navigate("/product/"+item?._id)} src={view} alt="view" />
+//               </button>
+//               <button className="border-0 bg-transparent">
+//                 <img onClick={() => navigate("/cart")} src={addcart} alt="addcart" />
+//               </button>
+              
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//         )
+//       })}  
+//     </>
+//   );
+// };
+
+// export default ProductCard;
+
+
 import React from "react";
 import ReactStars from "react-rating-stars-component";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import wish from "../images/wish.svg";
-import tshirt2 from "../images/vyamtshirt.png";
 import addcart from "../images/add-cart.svg";
 import view from "../images/view.svg";
 import { useDispatch } from "react-redux";
 import { addToWishlist } from "../features/products/productSlice";
+import styled from "styled-components"; // Import styled-components
+
+const ProductCardWrapper = styled.div`
+  padding: 15px;
+  background-color: white;
+  border-radius: 10px;
+  overflow: hidden;
+  width: 100%;
+  position: relative;
+  margin-bottom: 20px;
+`;
+
+const ProductImage = styled.div`
+  height: 270px;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const ProductDetails = styled.div`
+  h6 {
+    color: var(--color-bf4800);
+    font-size: 13px;
+  }
+  h5 {
+    font-size: 15px;
+    color: var(--color-1c1c1b);
+  }
+  p.price {
+    font-size: 16px;
+    color: var(--color-1c1c1b);
+  }
+  p.description {
+    font-size: 13px;
+    color: var(--color-777777);
+    margin-right: 20px;
+  }
+`;
+
+const ActionBar = styled.div`
+  position: absolute;
+  top: 50%;
+  right: -23px;
+  transform: translateY(-50%);
+  transition: 0.3s;
+`;
+
+const WishlistIcon = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
 
 const ProductCard = (props) => {
-  const {data, grid} = props;
+  const { data, grid } = props;
   const dispatch = useDispatch();
   let location = useLocation();
   const navigate = useNavigate();
- 
-  const addToWish =(id) =>{
+
+  const addToWish = (id) => {
     dispatch(addToWishlist(id));
   };
 
   return (
     <>
-      {data?.map((item,index) => {
-        return(
-          <div
+      {data?.map((item, index) => {
+        return (
+          <ProductCardWrapper
             key={index}
             className={` ${
               location.pathname === "/product" ? `gr-${grid}` : "col-3"
             } `}
           >
-        
-          <div
-          className="product-card position-relative"
-        >
-          <div className="wishlist-icon position-absolute">
-            <button 
-              className="border-0 bg-transparent" 
-              onClick={(e) =>{
-                addToWish(item?._id);
-              }}>
-              <img src={wish} alt="wishlist" />
-            </button>
-          </div>
-          <Link
-             to={`/product/${item._id}`}
-          >
-          <div className="product-image">
-          {
-            item?.images && item.images[0]&&(
-            <img src={item?.images[0].url} className="img-fluid" alt="product image" />
-              )
-          }
-          <img src={item?.images[0].url} className="img-fluid" alt="product image" />
-          </div>
-          </Link>
-          <div className="product-details">
-            <h6 className="brand">{item?.brand}</h6>
-            <Link
-              to={`/product/${item._id}`}
-            > 
-            <h5 className="product-title">
-             {item?.title}
-            </h5>
-            </Link>
-            
-            <ReactStars
-              count={5}
-              size={24}
-              value={item?.totalrating.toString()}
-              edit={false}
-              activeColor="#ffd700"
-            />
-            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}
-              dangerouslySetInnerHTML={{__html:item?.description }}
-            >
-            </p>
-            <p className="price">Rs {item?.price}</p>
-          </div>
-          <div className="action-bar position-absolute">
-            <div className="d-flex flex-column gap-15">
-              <button className="border-0 bg-transparent">
-                <img onClick={()=>navigate("/product/"+item?._id)} src={view} alt="view" />
-              </button>
-              <button className="border-0 bg-transparent">
-                <img onClick={() => navigate("/cart")} src={addcart} alt="addcart" />
-              </button>
-              
+            <div className="product-card position-relative">
+              <WishlistIcon>
+                <button
+                  className="border-0 bg-transparent"
+                  onClick={(e) => {
+                    addToWish(item?._id);
+                  }}
+                >
+                  <img src={wish} alt="wishlist" />
+                </button>
+              </WishlistIcon>
+              <Link to={`/product/${item._id}`}>
+                <ProductImage>
+                  {item?.images && item.images[0] && (
+                    <img
+                      src={item?.images[0].url}
+                      className="img-fluid"
+                      alt="product image"
+                    />
+                  )}
+                </ProductImage>
+              </Link>
+              <ProductDetails>
+                <h6 className="brand">{item?.brand}</h6>
+                <Link to={`/product/${item._id}`}>
+                  <h5 className="product-title">{item?.title}</h5>
+                </Link>
+                <ReactStars
+                  count={5}
+                  size={24}
+                  value={item?.totalrating.toString()}
+                  edit={false}
+                  activeColor="#ffd700"
+                />
+                <p
+                  className={`description ${
+                    grid === 12 ? "d-block" : "d-none"
+                  }`}
+                  dangerouslySetInnerHTML={{ __html: item?.description }}
+                ></p>
+                <p className="price">Rs {item?.price}</p>
+              </ProductDetails>
+              <ActionBar>
+                <div className="d-flex flex-column gap-15">
+                  <button className="border-0 bg-transparent">
+                    <img
+                      onClick={() => navigate("/product/" + item?._id)}
+                      src={view}
+                      alt="view"
+                    />
+                  </button>
+                  <button className="border-0 bg-transparent">
+                    <img
+                      onClick={() => navigate("/cart")}
+                      src={addcart}
+                      alt="addcart"
+                    />
+                  </button>
+                </div>
+              </ActionBar>
             </div>
-          </div>
-        </div>
-      </div>
-        )
-      })}  
+          </ProductCardWrapper>
+        );
+      })}
     </>
   );
 };
 
 export default ProductCard;
+
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -19,156 +19,30 @@ const OurStore = () => {
     dispatch(getAllProducts());
   };
   const totalproducts = productState.length;
-  const [selectedSorting, setSelectedSorting] = useState("manual");
-  const sortedProducts = [...productState];
-  if (selectedSorting === "title-ascending") {
-    sortedProducts.sort((a, b) => a.title.localeCompare(b.title));
-  } else if (selectedSorting === "title-descending") {
-    sortedProducts.sort((a, b) => b.title.localeCompare(a.title));
-  } else if (selectedSorting === "price-ascending") {
-    sortedProducts.sort((a, b) => a.price - b.price);
-  } else if (selectedSorting === "price-descending") {
-    sortedProducts.sort((a, b) => b.price - a.price);
-  } else if (selectedSorting === "created-ascending") {
-    sortedProducts.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-  } else if (selectedSorting === "created-descending") {
-    sortedProducts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-  }
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 320) {
+        setGrid(6);
+      } else {
+        setGrid(12);
+      }
+    };
 
-  // const [selectedCategory, setSelectedCategory] = useState("");
-  // const uniqueCategores = [...new Set(productState.map(product =>product.category))];
-  
-  // const filteredProducts = selectedCategory
-  //   ? productState.filter(product => product.category === selectedCategory)
-  //   : productState;
-  
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <Meta title={"Our Store"} />
       <BreadCrumb title="Our Store" />
       <Container class1="store-wrapper home-wrapper-2 py-5">
         <div className="row">
-          {/* <div className="col-3">
-            <div className="filter-card mb-3">
-              <h3 className="filter-title">Shop By Categories</h3>
-              <div>
-                <ul className="ps-0">
-                  {
-                    uniqueCategores.map((category,index) =>(
-                      <li key={index}
-                        onClick={() => setSelectedCategory(category)}
-                        style={{ cursor: "pointer" }}
-                      >{category}</li>
-                    ))
-                  }
-                  
-                </ul>
-              </div>
-            </div>
-            <div className="filter-card mb-3">
-              <h3 className="filter-title">Filter By</h3>
-              <div>
-                <h5 className="sub-title">Availablity</h5>
-                <div>
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value=""
-                      id=""
-                    />
-                    <label className="form-check-label" htmlFor="">
-                      In Stock 
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value=""
-                      id=""
-                    />
-                    <label className="form-check-label" htmlFor="">
-                      Out of Stock
-                    </label>
-                  </div>
-                </div>
-                <h5 className="sub-title">Price</h5>
-                <div className="d-flex align-items-center gap-10">
-                  <div className="form-floating">
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="floatingInput"
-                      placeholder="From"
-                    />
-                    <label htmlFor="floatingInput">From</label>
-                  </div>
-                  <div className="form-floating">
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="floatingInput1"
-                      placeholder="To"
-                    />
-                    <label htmlFor="floatingInput1">To</label>
-                  </div>
-                </div>
-                <h5 className="sub-title">Colors</h5>
-                <div>
-                  <Color />
-                </div>
-                <h5 className="sub-title">Size</h5>
-                <div>
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value=""
-                      id="color-1"
-                    />
-                    <label className="form-check-label" htmlFor="color-1">
-                      S (2)
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value=""
-                      id="color-2"
-                    />
-                    <label className="form-check-label" htmlFor="color-2">
-                      M (2)
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
           <div className="col-12">
             <div className="filter-sort-grid mb-4">
               <div className="d-flex justify-content-between align-items-center">
-                {/* <div className="d-flex align-items-center gap-10">
-                  <p className="mb-0 d-block" style={{ width: "100px" }}>
-                    Sort By:
-                  </p>
-                  <select
-                    value={selectedSorting} // Bind to the selectedSorting state
-                    onChange={(e) => setSelectedSorting(e.target.value)} // Update the selectedSorting state
-                    className="form-control form-select"
-                  >
-                    
-                    <option value="title-ascending">Alphabetically, A-Z</option>
-                    <option value="title-descending">
-                      Alphabetically, Z-A
-                    </option>
-                    <option value="price-ascending">Price, low to high</option>
-                    <option value="price-descending">Price, high to low</option>
-                    <option value="created-ascending">Date, old to new</option>
-                    <option value="created-descending">Date, new to old</option>
-                  </select>
-                </div> */}
+                
                 <div className="d-flex align-items-center gap-10">
                   <p className="totalproducts mb-0">{totalproducts}Products</p>
                   <div className="d-flex gap-10 align-items-center grid">
@@ -209,18 +83,18 @@ const OurStore = () => {
                 </div>
               </div>
             </div>
-            {/* <div className="products-list pb-5">
-              <div className="d-flex gap-10 flex-wrap">
-                {sortedProducts.map((product, index) => (
-                  <ProductCard key={index} data={product} grid={grid} />
-                ))}
-              </div>
-            </div> */}
             <div className="products-list pb-5">
-              <div className="d-flex gap-10 flex-wrap">
+              <div className={`row gr-8`}>
                       
-                
-                    <ProductCard data={productState} grid ={grid}  />
+                    {
+                      productState.map((item,index) =>(
+                        <div key={index} className={`col-3 gr-${grid}`}>
+                          <ProductCard  data={[item]} grid ={grid}  />
+                        </div>
+                        
+                      ))
+                    }
+                    
               </div>
             </div>
           </div>
