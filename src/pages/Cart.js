3f4fb016e6d860c61255efd570f4425b2462de67
Nauraj <lamjalnauraj14@gmail.com>
2023-11-10 20:48:06 +0545

    currency

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -6,68 +6,74 @@ import { AiFillDelete } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import Container from "../components/Container";
 import { useDispatch, useSelector } from "react-redux";
-import { clearUserCart, deleteCartProduct, getUserCart, updateCartProduct } from "../features/user/userSlice";
-import emptyCart from "../images/empty-cart.jpg"
+import {
+  clearUserCart,
+  deleteCartProduct,
+  getUserCart,
+  updateCartProduct,
+} from "../features/user/userSlice";
+import emptyCart from "../images/empty-cart.jpg";
 import { ConvertToPound } from "../components/ConvertToPound";
 const Cart = () => {
   const dispatch = useDispatch();
-  React.useEffect(() =>{
-    dispatch(getUserCart())
-  },[])
-  
-  const [productUpdateDetail,setProductUpdateDetail] = useState(null);
-  const [totalAmount,setTotalAmount] = useState(null);
-  const useCartState = useSelector((state) => state.auth.cartProducts)
-  
-  const userId = useSelector((state) =>state.auth.user._id);
+  React.useEffect(() => {
+    dispatch(getUserCart());
+  }, []);
 
-  useEffect(() =>{
-    if(productUpdateDetail !== null){
-      dispatch(updateCartProduct({cartItemId: productUpdateDetail?.cartItemId, quantity:productUpdateDetail?.quantity}))
-    setTimeout(() =>{
-      dispatch(getUserCart())
-    },200)
-      
+  const [productUpdateDetail, setProductUpdateDetail] = useState(null);
+  const [totalAmount, setTotalAmount] = useState(null);
+  const useCartState = useSelector((state) => state.auth.cartProducts);
+
+  const userId = useSelector((state) => state.auth.user._id);
+
+  useEffect(() => {
+    if (productUpdateDetail !== null) {
+      dispatch(
+        updateCartProduct({
+          cartItemId: productUpdateDetail?.cartItemId,
+          quantity: productUpdateDetail?.quantity,
+        })
+      );
+      setTimeout(() => {
+        dispatch(getUserCart());
+      }, 200);
     }
-  },[productUpdateDetail])
+  }, [productUpdateDetail]);
 
-  const deleteACartProduct =(id) =>{
-    dispatch(deleteCartProduct(id))
-    setTimeout(() =>{
-      dispatch(getUserCart())
-    },200)
-  }
+  const deleteACartProduct = (id) => {
+    dispatch(deleteCartProduct(id));
+    setTimeout(() => {
+      dispatch(getUserCart());
+    }, 200);
+  };
 
-  
-  
   const currency = useSelector((state) => state.currency.currency);
   const [convertedPrices, setConvertedPrices] = useState([]);
-  const [convertedSubtotal,setConvertedSubTotal] = useState(null);
+  const [convertedSubtotal, setConvertedSubTotal] = useState(null);
 
-  useEffect (() => {
+  useEffect(() => {
     let subTotal = 0;
     for (let index = 0; index < useCartState?.length; index++) {
-      subTotal += (Number(useCartState[index].quantity)*useCartState[index].price)  
+      subTotal +=
+        Number(useCartState[index].quantity) * useCartState[index].price;
     }
     setTotalAmount(subTotal);
-    
-      const conversionPrmoiseTotal = [ConvertToPound(subTotal)]
-      Promise.all(conversionPrmoiseTotal)
-        .then((converted) =>{
-          setConvertedSubTotal(converted[0]);
-        })
-        .catch((error) => console.error("Conversion error:" , error));
-      const conversionPromise = useCartState.map((item) => 
-        ConvertToPound(item?.price)
-      );
-      Promise.all(conversionPromise)
-        .then((conversionPrices) => {
-          setConvertedPrices(conversionPrices);
-          
-        })
-        .catch((error) => console.error("Conversion error" , error));
-    
-  },[currency,useCartState]);
+
+    const conversionPrmoiseTotal = [ConvertToPound(subTotal)];
+    Promise.all(conversionPrmoiseTotal)
+      .then((converted) => {
+        setConvertedSubTotal(converted[0]);
+      })
+      .catch((error) => console.error("Conversion error:", error));
+    const conversionPromise = useCartState?.map((item) =>
+      ConvertToPound(item?.price)
+    );
+    Promise.all(conversionPromise)
+      .then((conversionPrices) => {
+        setConvertedPrices(conversionPrices);
+      })
+      .catch((error) => console.error("Conversion error", error));
+  }, [currency, useCartState]);
   return (
     <>
       <Meta title={"Cart"} />
@@ -75,148 +81,158 @@ const Cart = () => {
       <Container class1="cart-wrapper home-wrapper-2 py-5">
         <div className="row">
           <div className="col-12">
-            { useCartState?.length === 0? (
+            {useCartState?.length === 0 ? (
               <div className="text-center">
                 <p>Your cart is empty. Add items to start shopping!</p>
-                <img
-                  src={emptyCart}
-                  alt="Emptry Cart"
-                  className="img-fluid"
-                />
+                <img src={emptyCart} alt="Emptry Cart" className="img-fluid" />
               </div>
-            ):(
-          <div>
-              <div className="cart-header py-3 d-flex justify-content-between align-items-center">
+            ) : (
+              <div>
+                <div className="cart-header py-3 d-flex justify-content-between align-items-center">
                   <h4 className="cart-col-1">Product</h4>
                   <h4 className="cart-col-2">Price</h4>
                   <h4 className="cart-col-3">Quantity</h4>
                   <h4 className="cart-col-4">Total</h4>
-              </div>
-            {
-              useCartState && useCartState?.map((item,index) =>{
-                if(item?.productId !== null){
-                  return(
-                    <div key={index} className="cart-data py-1 mb-1 d-flex justify-content-between align-items-center">
-                  <div className="cart-col-1 gap-15 d-flex align-items-center">
-                  <div className="w-70">
-                        {
-                      item?.productId.images && item.productId.images[0]&&(
-                        <img src={item?.productId.images[0].url} className="img-fluid" alt="product image" />
-                      )
-                    }
-                    
-                  </div>
-                  <div className="w-30">
-                    <p>{item?.productId.title}</p>
-                    
-                    <p className="d-flex gap-2">
-                      Color:
-                      <ul className="colors ps-0">
-                        {item?.productId.color.map((color, index) => (
-                          <li key={index} style={{ backgroundColor: color }}></li>
-                        ))}
-                      </ul>
-                    </p>
-                    <p> Size : {item?.size}</p>
-                  </div>
                 </div>
-                <div className="cart-col-2">
-                  <h5 className="price">
-                  {/* {currency === "Rs"
+                {useCartState &&
+                  useCartState?.map((item, index) => {
+                    if (item?.productId !== null) {
+                      return (
+                        <div
+                          key={index}
+                          className="cart-data py-1 mb-1 d-flex justify-content-between align-items-center"
+                        >
+                          <div className="cart-col-1 gap-15 d-flex align-items-center">
+                            <div className="w-70">
+                              {item?.productId.images &&
+                                item.productId.images[0] && (
+                                  <img
+                                    src={item?.productId.images[0].url}
+                                    className="img-fluid"
+                                    alt="product image"
+                                  />
+                                )}
+                            </div>
+                            <div className="w-30">
+                              <p>{item?.productId.title}</p>
+
+                              <p className="d-flex gap-2">
+                                Color:
+                                <ul className="colors ps-0">
+                                  {item?.productId.color.map((color, index) => (
+                                    <li
+                                      key={index}
+                                      style={{ backgroundColor: color }}
+                                    ></li>
+                                  ))}
+                                </ul>
+                              </p>
+                              <p> Size : {item?.size}</p>
+                            </div>
+                          </div>
+                          <div className="cart-col-2">
+                            <h5 className="price">
+                              {/* {currency === "Rs"
                   ? `Rs ${item?.price}`
                   : `£${convertedPrices[index]}`} */}
-                    £ {item?.price} /<br/>
-                    Rs {convertedPrices[index]}
-
-                  </h5>
-                </div>
-                <div className="cart-col-3 d-flex align-items-center gap-20">
-                  <div>
-                    <input
-                      className="form-control"
-                      type="number"
-                      // name=""
-                      min={1}
-                      max={10}
-                      id=""
-                      value={productUpdateDetail?.quantity ? productUpdateDetail?.quantity : item?.quantity}
-                      onChange={(e) => {setProductUpdateDetail({cartItemId:item?._id,quantity:e.target.value})}}
-                      disabled
-                    />
-                  </div>
-                  <div>
-                    <AiFillDelete onClick={() =>{deleteACartProduct(item?._id)}} className="text-danger " />
-                  </div>
-                </div>
-                <div className="cart-col-4">
-                
-                  <h5 className="price">
-                  {/* {currency === "Rs"
+                              £ {item?.price} /<br />
+                              Rs {convertedPrices[index]}
+                            </h5>
+                          </div>
+                          <div className="cart-col-3 d-flex align-items-center gap-20">
+                            <div>
+                              <input
+                                className="form-control"
+                                type="number"
+                                // name=""
+                                min={1}
+                                max={10}
+                                id=""
+                                value={
+                                  productUpdateDetail?.quantity
+                                    ? productUpdateDetail?.quantity
+                                    : item?.quantity
+                                }
+                                onChange={(e) => {
+                                  setProductUpdateDetail({
+                                    cartItemId: item?._id,
+                                    quantity: e.target.value,
+                                  });
+                                }}
+                                disabled
+                              />
+                            </div>
+                            <div>
+                              <AiFillDelete
+                                onClick={() => {
+                                  deleteACartProduct(item?._id);
+                                }}
+                                className="text-danger "
+                              />
+                            </div>
+                          </div>
+                          <div className="cart-col-4">
+                            <h5 className="price">
+                              {/* {currency === "Rs"
                   ? `Rs ${item?.price * item?.quantity}`
                   : `£${convertedPrices[index] * item?.quantity}`} */}
-                    £ {item?.price * item?.quantity} / <br/>
-                    Rs {convertedPrices[index] * item?.quantity}
-                    </h5>
-                </div>
-              </div>
-                     
-                  );
-                }
-                return null ;
-              })}
+                              £ {item?.price * item?.quantity} / <br />
+                              Rs {convertedPrices[index] * item?.quantity}
+                            </h5>
+                          </div>
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
 
-                   
-          {
-            useCartState && useCartState?.map((item,index) =>{
-              if(item?.productId === null){
-                return(
-                                  
-                <div className="text-center">
-                <p> Another Product you have added doesn't exist Please Clear the cart to continue !</p>
-                
-                <button 
-                className="button"
-                onClick={() => dispatch(clearUserCart(userId))}
-                >Clear Cart</button>
-              </div>
-                )
-              }
-              return null;
-            })
-          }
-          <div className="col-20 py-10 mt-2">
-          
-            <div className="d-flex justify-content-between align-items-baseline">
-                
-              
-              {
-                (totalAmount !== null || totalAmount !== 0) &&
-                <div className="d-flex flex-column ">
-                <h4>SubTotal: 
-                  {/* {
+                {useCartState &&
+                  useCartState?.map((item, index) => {
+                    if (item?.productId === null) {
+                      return (
+                        <div className="text-center">
+                          <p>
+                            {" "}
+                            Another Product you have added doesn't exist Please
+                            Clear the cart to continue !
+                          </p>
+
+                          <button
+                            className="button"
+                            onClick={() => dispatch(clearUserCart(userId))}
+                          >
+                            Clear Cart
+                          </button>
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
+                <div className="col-20 py-10 mt-2">
+                  <div className="d-flex justify-content-between align-items-baseline">
+                    {(totalAmount !== null || totalAmount !== 0) && (
+                      <div className="d-flex flex-column ">
+                        <h4>
+                          SubTotal:
+                          {/* {
                     currency === "Rs" ? `Rs ${totalAmount}`:`£ ${convertedSubtotal}`
                   } */}
-                  £ {totalAmount} / Rs {convertedSubtotal}
-                  </h4>
-                <p>Taxes and shipping calculated at checkout</p>
-                
-                
-              </div>
-              }
-              <Link to="/checkout" className="button">
-                  Checkout
-                </Link>
-            </div>
-            
-          </div>
-          
+                          £ {totalAmount} / Rs {convertedSubtotal}
+                        </h4>
+                        <p>Taxes and shipping calculated at checkout</p>
+                      </div>
+                    )}
+                    <Link to="/checkout" className="button">
+                      Checkout
+                    </Link>
+                  </div>
+                </div>
               </div>
             )}
             <Link to="/product" className="button ">
               Continue To Shopping
             </Link>
           </div>
-          
         </div>
       </Container>
     </>
