540778a8b8b6483deb0a4e7a24d59163cb5c1580
Aran Shrestha <aranstha10@gmail.com>
2023-11-25 07:43:36 +0545

    CSS

--- src/components/BigBanner.js
+++ src/components/BigBanner.js
@@ -39,30 +39,66 @@ const StyledCard = styled(Card)`
 `;
 const BigBanner = ({ item }) => {
   return (
-    <>
-      {item ? (
-        <StyledCard sx={{ maxWidth: 320 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              sx={{ height: 165 }}
-              image={item?.images[0].url}
-              alt="img"
+    
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#c9c9c9",
+        padding: "5px",
+        borderRadius: "5px",
+      }}
+    >
+      {item? (
+        <Link to={`/product/${item?._id}`}>
+
+        <div style={{ flex: "4" }}>
+          {item?.images && item.images[0] && (
+            
+            <img
+              src={item?.images[0].url}
+              className="img-fluid rounded-3 banner-image"
+              alt="main banner"
             />
-            <CardContent>
-              <Typography gutterBottom variant="body2" component="div">
-                {item?.title}
-              </Typography>
-              <Button size="small" color="primary">
-                <Link to={`/product/${item?._id}`}>Buy Now</Link>
-              </Button>
-            </CardContent>
-          </CardActionArea>
-        </StyledCard>
+          )}
+        </div>
+        </Link>
       ) : (
         <BigBannerSkeletonWrapper />
       )}
-    </>
+
+      {item && (
+        
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            borderRadius: "5px",
+          }}
+        >
+          <h5>{item?.title}</h5>
+
+          <Link
+            to={`/product/${item?._id}`}
+            style={{
+              backgroundColor: "black",
+              color: "white",
+              padding: "8px 15px",
+              borderRadius: "5px",
+              fontSize: "8px",
+              marginBottom: "5px",
+            }}
+          >
+            BUY NOW
+          </Link>
+        </div>
+      )}
+    </div>
   );
 };
 
