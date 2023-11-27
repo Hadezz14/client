445175d1ddc73c0299027d095fde367b8e6e60f3
root <root@ubuntu-cloudpanel.localhost>
2023-11-27 02:11:58 +0000

    fromServer

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,6 @@
 // export const base_url ="https://vyam-backend.onrender.com/api/";
-// export const base_url ="https://api.vyamstore.com/api/";
-export const base_url = "http://localhost:5069/api/";
+ export const base_url ="https://api.vyamstore.com/api/";
+//export const base_url = "http://localhost:5069/api/";
 
 const clearTokenFromLocalStorage = () => {
   if (typeof localStorage !== "undefined") {
