5cda3713119f3c11d16bea54db013b77c704dd9e
Nauraj <lamjalnauraj14@gmail.com>
2023-12-09 20:32:23 +0545

    Currency_update

--- src/features/currency/currencyService.js
+++ src/features/currency/currencyService.js
@@ -1,24 +1,20 @@
+import axios from "axios";
+import React from "react";
 
-import axios from 'axios';
-import React from 'react'
-
-const API_KEY = "4203ccb753363c94e8063641";
+const API_KEY = "d5c15adc492ec0b57507fe4a";
 let conversionRate = null;
 
 export const getRsToPoundConversionrate = async () => {
   try {
-    if(conversionRate!== null) {
-      return conversionRate;
-    }
-      const response = await  axios.get(
-      `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/GBP`
+    if (conversionRate === null) {
+      const response = await axios.get(
+        `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/GBP`
       );
-      
-    conversionRate = response.data.conversion_rates.NPR;
-   return conversionRate;
+      conversionRate = response.data.conversion_rates.NPR;
+    }
+    return conversionRate;
   } catch (error) {
-    console.error("Error fetching converison rate:", error)
-    throw error
+    console.error("Error fetching converison rate:", error);
+    throw error;
   }
-}
-
+};
